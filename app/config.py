7fd1a88abbac578c3a925854b33adb007e8814b8
Aasf-Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-03-20 10:51:26 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,33 @@
+"""
+MIT License
+Copyright (C) 2021-2022, NkSama
+Copyright (C) 2021-2022 Moezilla
+Copyright (c) 2021, Sylviorus, <https://github.com/Sylviorus/BlueMoonVampireBot>
+This file is part of @BlueMoonVampireBot (Antispam Telegram Bot)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+API_ID = "12101541"
+API_HASH = "937c9b43519a84a4972c0e19821cbb78"
+BOT_TOKEN = "5163902130:AAG8urBLGSYRyO4Jf5xUa1eL5mHIs0zaxho"
+SLYTOKEN = "idk"
+MONGO_URI = "mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority"
+REPORT_LOGS = "-1001781501832"
+LOGS = "-1001781501832"
+DEV = [
+ 5175767264, 1989750989
+]
